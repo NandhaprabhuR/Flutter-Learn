3f4c8ef3fbcc39fe5fb198f2fb5c81ec51f1b0de
nandhaprabhurzenthink <nandhaprabhur.zenthink@gmail.com>
2026-02-04 23:09:43 +0530

    learned home property bg color, appbar, runapp and scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
-
+// import 'package:flutter/dart:ui';
 void main() {
-}
+  runApp(
+
+    MaterialApp(debugShowCheckedModeBanner: false,
+      home: Scaffold(
+
+        backgroundColor: Colors.yellow,
+        appBar: AppBar(title: Text("swiggy"),backgroundColor: Colors.amber,
+        centerTitle: false,),),),
+
+      );
+
+    }
+
+
+
+
