02a5e7d3177b4508a3b9c1f96de349ef56302dd3
nandhaprabhurzenthink <nandhaprabhur.zenthink@gmail.com>
2026-02-12 14:04:27 +0530

    container with box shadow

--- lib/main.dart
+++ lib/main.dart
@@ -9,39 +9,45 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration
-              (borderRadius:BorderRadius.circular(40),color: Colors.cyan,),
-            child:  Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [Container(
-              height: 100,width: 100,
-              decoration: BoxDecoration(borderRadius:BorderRadius.circular(10),color: Colors.red),
-              child: Text("hello"),alignment: Alignment.center,
-              padding: EdgeInsets.all(15),
+    return  MaterialApp(debugShowCheckedModeBanner: false,
+      home: Scaffold(backgroundColor: Colors.brown,
+          appBar: AppBar(backgroundColor:Colors.white,title: Text("Restarunt menu",textAlign: TextAlign.center,)),
+          body:Container(decoration: BoxDecoration(color: Colors.white,boxShadow: [BoxShadow(blurRadius: 20,spreadRadius: 1)],),height:100,width:double.infinity,margin:EdgeInsets.all(10),child: Column(children: [Text("chicken noodles"),Text("delicious chicken noodles"),Text("29")],))
+        // Container
+      ),
+      // Center
 
-            ),
-              Container(
-              height: 100,width: 100,
-              decoration: BoxDecoration(borderRadius:BorderRadius.circular(10),color: Colors.red),
-              child: Text("hello"),alignment: Alignment.centerRight,
-              padding: EdgeInsets.all(15),),
-
-
-]
-            )
-
-
-          ), // Containerc
-        ), // Center
-      ), // Scaffold
     ); // MaterialApp
   }
 }
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MainApp());
+// }
+//
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return  MaterialApp(debugShowCheckedModeBanner: false,
+//       home: Scaffold(backgroundColor: Colors.brown,
+//         appBar: AppBar(backgroundColor:Colors.white,title: Text("Restarunt menu",textAlign: TextAlign.center,)),
+//         body:Container(decoration: BoxDecoration(color: Colors.white,boxShadow: [BoxShadow(blurRadius: 20,spreadRadius: 1)],),height:100,width:double.infinity,margin:EdgeInsets.all(10),child: Column(children: [Text("chicken noodles"),Text("delicious chicken noodles"),Text("29")],))
+//           // Container
+//       ),
+//     // Center
+//
+//     ); // MaterialApp
+//   }
+// }
